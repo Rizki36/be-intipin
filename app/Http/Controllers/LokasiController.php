afbf9f7ff929b31bcb24ad2cc0881bc894d8822e
Rizki36 <cahrizki.rf@gmail.com>
2022-05-19 00:50:52 +0700

    feat: create resource lokasi

---
+++ app/Http/Controllers/LokasiController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lokasi;
+use App\Http\Requests\StoreLokasiRequest;
+use App\Http\Requests\UpdateLokasiRequest;
+
+class LokasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreLokasiRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreLokasiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lokasi  $lokasi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lokasi $lokasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lokasi  $lokasi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lokasi $lokasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateLokasiRequest  $request
+     * @param  \App\Models\Lokasi  $lokasi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateLokasiRequest $request, Lokasi $lokasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lokasi  $lokasi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lokasi $lokasi)
+    {
+        //
+    }
+}
