73404c72b379e5514c7627973f6a6f986e2f0cfb
Rizki36 <cahrizki.rf@gmail.com>
2022-05-20 21:51:06 +0700

    add tooltip

--- resources/views/admin/admin/_action.blade.php
+++ resources/views/admin/admin/_action.blade.php
@@ -1,7 +1,7 @@
-<a class="btn btn-sm btn-warning" href="{{ route('admin.edit', ['admin' => $id]) }}">
+<a title="Update Data" class="btn btn-sm btn-warning" href="{{ route('admin.edit', ['admin' => $id]) }}">
     <i class="bi bi-pencil"></i>
 </a>
 
-<a class="btn btn-sm btn-danger btn-delete" href="{{ route('admin.destroy', ['admin' => $id]) }}">
+<a title="Hapus Data" class="btn btn-sm btn-danger btn-delete" href="{{ route('admin.destroy', ['admin' => $id]) }}">
     <i class="bi bi-trash"></i>
 </a>
