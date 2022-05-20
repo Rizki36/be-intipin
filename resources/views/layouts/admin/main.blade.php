a731881a69b299b968eff2935ff4c97806cfef6b
Rizki36 <cahrizki.rf@gmail.com>
2022-05-20 20:03:04 +0700

    refactor: change path to be more standard

--- resources/views/layouts/admin/main.blade.php
+++ resources/views/layouts/admin/main.blade.php
@@ -127,7 +127,7 @@
     <script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.0/js/dataTables.bootstrap5.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
-    <script src="{{ asset('vendor\datatables\buttons.server-side.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
 
     @stack('scripts')
 </body>
