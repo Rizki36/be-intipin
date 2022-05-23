a7c72d92cb411a0baa4ce26500ba9686320fa9eb
Rizki36 <cahrizki.rf@gmail.com>
2022-05-23 16:10:20 +0700

    feat: enable login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,40 +5,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Intipin Jombang</title>
-    <link rel="stylesheet" href="{{ asset("assets/admin/css/main/app.css") }}">
-    <link rel="stylesheet" href="{{ asset("assets/admin/css/pages/auth.css") }}">
-    <link rel="shortcut icon" href="{{ asset("assets/admin/images/logo/favicon.svg") }}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset("assets/admin/images/logo/favicon.png") }}" type="image/png">
+    <link rel="stylesheet" href="{{ asset('assets/admin/css/main/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/admin/css/pages/auth.css') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/admin/images/logo/favicon.svg') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/admin/images/logo/favicon.png') }}" type="image/png">
 </head>
 
 <body>
-    <div id="auth">
-        
-<div class="row h-100 justify-content-center">
-    <div class="col-lg-5 col-12">
-        <div id="auth-left">
-            <div class="auth-logo">
-                <a href="index.html"></a>
+    <div id="auth" style="height: 100vh">
+        <div class="row h-100 justify-content-center">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                    </div>
+                    <h1 class="auth-title">Log in</h1>
+                    <form action="{{ route('auth.loginAction') }}" method="POST">
+                        @csrf
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input name="email" type="text" class="form-control form-control-xl" placeholder="Email"
+                                required>
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input name="password" type="password" class="form-control form-control-xl"
+                                placeholder="Password" required>
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        @if (session('error'))
+                            <div class="alert alert-light-danger color-danger">
+                                <i class="bi bi-exclamation-circle"></i>
+                                {{ session('error') }}
+                            </div>
+                        @endif
+
+                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                    </form>
+                </div>
             </div>
-            <h1 class="auth-title">Log in</h1>
-            <form action="index.html">
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="text" class="form-control form-control-xl" placeholder="Username" required>
-                    <div class="form-control-icon">
-                        <i class="bi bi-person"></i>
-                    </div>
-                </div>
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="password" class="form-control form-control-xl" placeholder="Password" required>
-                    <div class="form-control-icon">
-                        <i class="bi bi-shield-lock"></i>
-                    </div>
-                </div>
-                <a href="{{ url("admin") }}" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</a>
-            </form>
         </div>
-    </div>
-</div>
 
     </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\KecamatanController;
 use App\Http\Controllers\KelurahanController;
@@ -30,38 +31,43 @@ Route::get('/produk', function () {
     return view('produk');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
+// login
+Route::get('/login', [AuthController::class, 'loginView'])->name('login');
+Route::post('/login', [AuthController::class, 'loginAction'])->name('auth.loginAction');
+Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
-Route::get('/admin', function () {
-    return view('admin.dashboard');
-});
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
 
-Route::resource('admin/admin', AdminController::class);
-Route::get('admin/admin/datatable', [AdminController::class, 'datatable'])->name('admin.datatable');
+    // admin
+    Route::resource('admin/admin', AdminController::class);
+    Route::get('admin/admin/datatable', [AdminController::class, 'datatable'])->name('admin.datatable');
 
-Route::put('admin/setting/all', [SettingController::class, 'update_all'])->name('setting.update_all');
-Route::resource('/admin/setting', SettingController::class)->parameter('setting', 'id');
-Route::resource('/admin/gallery', GalleryController::class)->parameter('gallery', 'id');
+    // setting
+    Route::put('admin/setting/all', [SettingController::class, 'update_all'])->name('setting.update_all');
+    Route::resource('/admin/setting', SettingController::class)->parameter('setting', 'id');
+    Route::resource('/admin/gallery', GalleryController::class)->parameter('gallery', 'id');
 
-Route::get('/admin/lokasi', function () {
-    return view('admin.lokasi');
-});
+    // lokasi
+    Route::get('/admin/lokasi', function () {
+        return view('admin.lokasi');
+    });
 
+    Route::controller(KecamatanController::class)->group(function () {
+        // menampilkan data kecamatan berdasarkan id
+        Route::get('/kecamatan/{id}', 'show');
 
-Route::controller(KecamatanController::class)->group(function () {
-    // menampilkan data kecamatan berdasarkan id
-    Route::get('/kecamatan/{id}', 'show');
+        // menampilkan semua data kecamatan
+        Route::get('/kecamatan', 'index');
+    });
 
-    // menampilkan semua data kecamatan
-    Route::get('/kecamatan', 'index');
-});
+    Route::controller(KelurahanController::class)->group(function () {
+        // menampilkan semua data kelurahan
+        Route::get('/kelurahan', 'index');
 
-Route::controller(KelurahanController::class)->group(function () {
-    // menampilkan semua data kelurahan
-    Route::get('/kelurahan', 'index');
-
-    // menampilkan data kelurahan berdasarkan id
-    Route::get('/kelurahan/{id}', 'show');
+        // menampilkan data kelurahan berdasarkan id
+        Route::get('/kelurahan/{id}', 'show');
+    });
 });
